1e4d4a53da6a647424376af7191173ff57eaa9ba
Jean Carlos <scheunemanngame@gmail.com>
2017-07-26 17:15:52 -0300

    Implementação da nova tenica que analisa apenas as regioes de ocorrencia de erros

--- erro.m
+++ erro.m
@@ -6,6 +6,7 @@ pkg load image
 
 plots=0;
 WSize=64; %tamanho da janela de analise
+borderSize=20
 
 a=imread("layout.png");	%load img alvo
 abw=rgb2gray(a);  %converte para B&W
@@ -21,42 +22,48 @@ if(plots)
 	subplot(3,1,2);imshow(B);title("Layout produzido");
 	subplot(3,1,3);imshow(((B-A)+1)./2);title("Erros detectados"); hold on;
 end
-%%
+%
 figure; imshow(B)
-n_deep=4;
+rompimentos=0;
+curtos=0;
 if(sum(sum(abs(B-A))))
-	disp("Erros foram encontrados, iniciando deteccao de localizacao");
+	disp("\n\n\nErros foram encontrados, iniciando deteccao de localizacao");
 	Dif=B-A;
+	[y x]= size(Dif);
 	#tentativa de encontrar a regiao dos erros
 	M=zeros(20,20);
 	M(6:15,6:15)=1;
-	tmp=conv2(abs(Dif),M);
-	[tmp m]=bwlabel(tmp);
-	disp(["Encontrados ",num2str(m)," erros"]);
-	
-	[y x]= size(Dif);
-	nx=floor(x/WSize);
-	ny=floor(y/WSize);
-	for j=1:nx
-		for k=1:ny
-			if(sum(sum(abs(Dif((k-1)*WSize+1:k*WSize,(j-1)*WSize+1:j*WSize)))))
-				[tmp m]=bwlabel(A((k-1)*WSize+1:k*WSize,(j-1)*WSize+1:j*WSize));
-				[tmp n]=bwlabel(B((k-1)*WSize+1:k*WSize,(j-1)*WSize+1:j*WSize));
-				[tmp p]=bwlabel(~A((k-1)*WSize+1:k*WSize,(j-1)*WSize+1:j*WSize));
-				[tmp q]=bwlabel(~B((k-1)*WSize+1:k*WSize,(j-1)*WSize+1:j*WSize));
-				color='g';
-				if(m>n)
-					color='b';
-				elseif(p>q)
-					color='r';
-				end				
-				rectangle('Position',[ ((j-1)*WSize+1) ((k-1)*WSize+1) (WSize) (WSize)],'EdgeColor',color);
-			end
+	tmp1=conv2(abs(Dif),M);
+	[b m]=bwboundaries(tmp1);
+	disp(["Encontrados ",num2str(length(b))," erros, Inicinado analise..."]);	
+	for i =1:length(b)
+		c1=max(b{i});
+		c2=min(b{i});
+		ya=(c2(1)-borderSize);
+		yb=(c1(1)+borderSize);
+		xa=(c2(2)-borderSize);
+		xb=(c1(2)+borderSize);
+		if(xb>x)
+			xb=x;
 		end
-		if(sum(sum(abs(Dif(y-WSize:y,(j-1)*WSize+1:j*WSize)))))
-			rectangle('Position',[ ((j-1)*WSize+1) ((k-1)*WSize+1) (WSize) (WSize)],'EdgeColor','r');
+		[tmp m]=bwlabel( A(ya:yb , xa:xb));
+		[tmp n]=bwlabel( B(ya:yb , xa:xb));
+		[tmp p]=bwlabel(~A(ya:yb , xa:xb));
+		[tmp q]=bwlabel(~B(ya:yb , xa:xb));
+		color='g';
+		if(m>n)
+			color='b';
+			rompimentos=rompimentos+1;
+		elseif(p>q)
+			color='r';
+			curtos=curtos+1;
 		end
+		p=[ (c2(2)-borderSize) (c2(1)-borderSize) ((c1(2)-c2(2))+borderSize) ((c1(1)-c2(1))+borderSize)];
+		rectangle('Position',p,'EdgeColor',color);
 	end
+	disp(["\t Rompimentos de trilhas:",num2str(rompimentos)]);
+	disp(["\t Curtos:",num2str(curtos)]);
+	disp(["\t Erros nao destrutivos:",num2str(length(b)-rompimentos-curtos)]);
 else
 	disp("Sem erros");
 end
